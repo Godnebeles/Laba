c20d332d37dc27d4ecc91cb2facb64abb6fa3001
Oleg Nevmitiy <nevmytyj.oleg@vu.cdu.ua>
2020-12-20 16:11:28 +0200

    Створено найпростіший консольний додаток

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LabaInform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
